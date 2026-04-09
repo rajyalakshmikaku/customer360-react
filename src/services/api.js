d5d84876545d315a39c60fa134f9abb64680d1ee
Rajyalakshmi Kaku <lakshmikaku2708@gmail.com>
2026-04-09 10:45:24 +0530

    api url

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import alertify from "alertifyjs";
 
 export const ResetURL = "http://localhost:5055";
 //export const baseURL = "http://localhost:5055";
-export const baseURL = "http://102.130.114.194:1510";
+export const baseURL = "http://102.130.114.194:1541";
 
 export const AxiosInstance = axios.create({
   baseURL: baseURL,
